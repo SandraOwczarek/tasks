4a384809c509a90a12697b7c10118c7adddeff58
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-04-17 19:08:55 +0200

    23.3 Carbon Copy

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Mail {
+    private final String mailTo;
+    private final String subject;
+    private final String message;
+    private final String toCc;
+}
