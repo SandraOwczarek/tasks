0c90638577ab05cce896cccd80cc8064d2d56551
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-06-19 20:31:28 +0200

    Zadanie 32.3

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -1,5 +1,4 @@
 package com.crud.tasks.scheduler;
-
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
@@ -7,32 +6,34 @@ import com.crud.tasks.service.SimpleEmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-
 @Component
 @RequiredArgsConstructor
 public class EmailScheduler {
-
     private static final String SUBJECT = "Tasks: Once a day email";
     private final SimpleEmailService simpleEmailService;
     private final TaskRepository taskRepository;
     private final AdminConfig adminConfig;
-
     @Scheduled(fixedDelay = 100000)/*(cron = "0 0 10 * * *")*/
     public void sendInformationEmail() {
         long size = taskRepository.count();
-        String taskNo = "tasks";
-        if(size == 1) taskNo = "task";
-
-
-        simpleEmailService.send(
+        simpleEmailService.sendOnceDay(
                 new Mail(
                         adminConfig.getAdminMail(),
                         SUBJECT,
-                        "Currently in database you got: " + size + " " + taskNo + ".",
+                        this.messageBody(size),
                         null
                 )
         );
+    }
+    private String messageBody(long size){
+        String body;
+        if (size==0){
+            body = "Currently in database you don't have any tasks";
+        } else if (size==1){
+            body = "Currently in database you got: " + size + " task";
+        } else {
+            body = "Currently in database you got: " + size + " tasks";
+        }
+        return body;
     }
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -3,10 +3,15 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -14,26 +19,44 @@ import org.springframework.stereotype.Service;
 public class SimpleEmailService {
 
     private final JavaMailSender javaMailSender;
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     public void send(final Mail mail) {
         log.info("Starting email preparation...");
         try {
-            SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
+            javaMailSender.send(createMimeMessage(mail));
             log.info("Email has been sent.");
         } catch (MailException e) {
             log.error("Failed to process email sending: " + e.getMessage(), e);
         }
     }
 
-    private SimpleMailMessage createMailMessage(final Mail mail) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(mail.getMailTo());
-        if (mail.getToCc() != null && !mail.getToCc().isEmpty()) {
-            mailMessage.setCc(mail.getToCc());
+    public void sendOnceDay(final Mail mail) {
+        log.info("Starting email preparation...");
+        try {
+            javaMailSender.send(createOnceDayMimeMessage(mail));
+            log.info("Email has been sent.");
+        } catch (MailException e) {
+            log.error("Failed to process email sending: " + e.getMessage(), e);
         }
-        mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
-        return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+    private MimeMessagePreparator createOnceDayMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildOnceDayMail(mail.getMessage()), true);
+        };
     }
 }
